66d6e55578c9902f79769e6339396237d1df5750
Forrest Alvarez <forrest.alvarez@gmail.com>
2013-09-09 06:29:35 +0000

    added config items

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,11 +19,11 @@ TRANSLATION_FEED_ATOM = None
 LINKS =  (('Pelican', 'http://getpelican.com/'),
           ('Python.org', 'http://python.org/'),
           ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+          ('SaltStack', 'http://saltstack.org'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'http://github.com/gravyboat'),
+          )
 
 DEFAULT_PAGINATION = 10
 
