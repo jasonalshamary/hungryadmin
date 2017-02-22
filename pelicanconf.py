f8ac9306cd43c528759c1cab7464e44f9554bf34
Forrest Alvarez <forrest.alvarez@gmail.com>
2017-02-21 18:42:57 -0800

    Wrong one

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,7 +39,7 @@ PAGE_PATHS = ['content/pages']
 
 # Theme
 THEME = "themes/pelican-themes/pelican-bootstrap3"
-BOOTSTRAP_THEME = "slate"
+BOOTSTRAP_THEME = "sandstone"
 
 # Code block theme
 PYGMENTS_STYLE = 'solarizeddark'
